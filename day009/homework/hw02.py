ae28baeb4ecabdc0254e698ce3a71f3bcc0ad12c
Aandria777 <korganashviliandria7@gmail.com>
2025-08-01 14:00:57 +0400

    bubu

---
+++ day009/homework/hw02.py
@@ -0,0 +1,5 @@
+temperature = 37
+if temperature > 20:
+    print("temperature is high")
+else:
+    print("temperature is low")
---
+++ day009/homework/hw04.py
@@ -0,0 +1,5 @@
+momxmareblis_tanxa = 20
+if momxmareblis_tanxa < 25:
+    print("momxmarebels ar aakvs sakmarisi tanxa")
+else:
+    print("momxmarebels akvs sakmarisi tanxa")
