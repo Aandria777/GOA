075b856545929611bca1a4c497b76863e25b07fc
Aandria777 <korganashviliandria7@gmail.com>
2025-09-12 20:55:21 +0400

    ai

---
+++ day021/classwork/cw01.py
@@ -0,0 +1,5 @@
+array = [1, 2, 3, 4, 5, "andria", 1.532, 23, "lomi", 2.44 ]
+print (len(array))
+
+for i in range(len(array)):
+    print({array[i]})
---
+++ day021/classwork/cw02.py
@@ -0,0 +1,3 @@
+reversed_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+reversed_array = reversed_array[::-1]
+print(reversed_array)
