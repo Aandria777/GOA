7548757ac3e2bb2880f6c13a44827db4791d5d20
andria korganashvili <korganashviliandria7@gmail.com>
2025-10-25 11:52:39 +0400

    gut

---
+++ day033/classwork/cw01.py
@@ -0,0 +1,5 @@
+def to_celsius(f):
+    return 5/9 * (f - 32)
+
+print(to_celsius(90))
+print(to_celsius(32))
---
+++ day033/classwork/hw02.py
@@ -0,0 +1,3 @@
+def calculate_damage(opening_attack, core_damage, finishing_move):
+    return f"wizzard new life is{opening_attack + core_damage + finishing_move}"
+print(calculate_damage(10, 20, 30))
